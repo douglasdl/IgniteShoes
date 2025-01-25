52ed95f586d135de5cea85824c65ee67593b29d7
Douglas Dias Leal <douglas_san@hotmail.com>
2025-01-26 08:47:46 +0900

    feat: add BrandProps

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -1,7 +1,7 @@
-import { FlatList } from 'native-base';
-
-import { Brand } from './Brand';
-import { BRANDS } from '../data/brands';
+import { FlatList } from '@gluestack-ui/themed'
+import { Brand } from './Brand'
+import { BRANDS, BrandProps } from '../data/brands'
+import { ImageSourcePropType } from 'react-native';
 
 type Props = {
   onSelect: (value: string) => void;
@@ -13,10 +13,10 @@ export function Brands({ onSelect, selected }: Props) {
   return (
     <FlatList
       data={BRANDS}
-      keyExtractor={item => item.id}
-      renderItem={({ item }) => (
+      keyExtractor={(item: BrandProps) => item.id}
+      renderItem={( item: BrandProps ) => (
         <Brand
-          image={item.image}
+          image={item.image as ImageSourcePropType}
           isActive={selected === item.name}
           onPress={() => onSelect(item.name)}
         />

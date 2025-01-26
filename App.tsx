5d05a3ed9995132fb482ccf401a7de88141cb2f1
Douglas Dias Leal <douglas_san@hotmail.com>
2025-01-26 15:46:11 +0900

    feat: add environment variables

--- App.tsx
+++ App.tsx
@@ -1,21 +1,34 @@
-import { StatusBar } from 'react-native';
+import { StatusBar, Platform } from 'react-native';
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { OneSignal } from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
-import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserEmailCreate, tagUserEmailRemove, tagUserInfoCreate } from './src/notifications/notificationsTags';
+
+import { ONESIGNAL_APP_ID_IOS, ONESIGNAL_APP_ID_ANDROID, USER_EMAIL, USER_NAME } from '@env';
+
+const oneSignalAppId = Platform.OS === "ios" 
+  ? ONESIGNAL_APP_ID_IOS 
+  : ONESIGNAL_APP_ID_ANDROID;
+
+OneSignal.initialize(oneSignalAppId);
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
+  // tagUserEmailCreate(USER_EMAIL)
+  // tagUserEmailRemove()
+  tagUserInfoCreate({ name: USER_NAME, email: USER_EMAIL })
+
   return (
-    <GluestackUIProvider mode="light"><NativeBaseProvider theme={THEME}>
+    <GluestackUIProvider mode="light">
         <StatusBar
           barStyle="light-content"
           backgroundColor="transparent"
@@ -24,6 +37,6 @@ export default function App() {
         <CartContextProvider>
           {fontsLoaded ? <Routes /> : <Loading />}
         </CartContextProvider>
-      </NativeBaseProvider></GluestackUIProvider>
+      </GluestackUIProvider>
   );
 }
